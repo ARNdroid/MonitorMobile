7243ec070558100ae3c4d59f01ad5c2e85bacc0d
ARNdroid <arndroidbr@gmail.com>
2014-11-26 10:52:05 -0200

    @MacBook: Fix #33.

--- app/src/main/java/br/gov/caixa/monitormobile/AboutActivity.java
+++ app/src/main/java/br/gov/caixa/monitormobile/AboutActivity.java
@@ -1,24 +1,30 @@
 package br.gov.caixa.monitormobile;
 
+import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class DashboardActivity extends Activity {
+import br.gov.caixa.monitormobile.R;
+
+public class AboutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_about);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
 
@@ -26,19 +32,10 @@ public class DashboardActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         final int itemId = item.getItemId();
 
-        final Intent intent;
-
         switch (itemId) {
-            case R.id.action_simulations:
-                intent = new Intent(this, SimulationsActivity.class);
-                startActivity(intent);
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
                 return true;
-
-            case R.id.action_about:
-                intent = new Intent(this, AboutActivity.class);
-                startActivity(intent);
-                return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }
